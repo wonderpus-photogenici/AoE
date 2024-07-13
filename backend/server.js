127487004442ed21412d30a925b3422dfb642dc0
Kyler-Chiago <kchiago50@gmail.com>
2024-07-13 15:20:21 -0700

    addUser and verifyUser working with POSTMAN

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-// express
-
 const express = require('express'); 
-const App = express();
+const app = express();
 const path = require('path');
 const cors = require('cors'); 
 const cookieParser = require('cookie-parser');
@@ -9,54 +7,36 @@ const cookieParser = require('cookie-parser');
 
 const userController = require('./controllers/userController');
 
-App.use(cors());
-App.use(cookieParser());
-App.use(express.json());
-App.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
-// set up the static files for the entry of webpack path.join(direct path)
-App.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-//route for adding user to the database
-App.post('/api/register', userController.addUser, (req, res) => {
-    res.status(200).json({message: 'Successful Registration'})
+// route for adding user to the database
+app.post('/api/register', userController.addUser, (req, res) => {
+    // res.status(200).json({message: 'Successful Registration'});
     res.redirect('/login');
 })
 
-//autenticating users from the database; 
-App.post('/api/login', userController.verifyUser, (req, res) => {
-    res.status(200).json({message: 'Login Success'})
-    res.redirect('/home'); 
+// authenticating users from the database
+app.post('/api/login', userController.verifyUser, (req, res) => {
+    // res.status(200).json({message: 'Login Success'});
+    console.log('end of login');
+    res.redirect('/'); 
 })
 
-//User will be directed to the html login page. 
-App.get('*', (req, res) => {
+
+app.get('*', (req, res) => {
     return res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
 })
 
-// App.get('/home', (req, res) => {
-//     return res.status(200).sendFile('../src/index.html')
-// }) 
+// Unknown route error handler 
+app.use('*', (req, res) => res.status(404).sendFile('../src/index.html'));
 
-// App.get('/profile', (req, res) => {
-//     return res.status(200).sendFile('../src/index.html')
-// })
-
-// App.get('/signUp', (req, res) => {
-//     return res.status(200).sendFile('../src/index.html')
-// })
-
-
-
-
-
-//Unknown route error handler 
-App.use('*', (req, res) => res.status(404).sendFile('../src/index.html'))
-
-
-
-//Global error handler
-App.use((err, req, res, next) => {
+// Global error handler
+app.use((err, req, res, next) => {
     const defaultErr = {
       log: 'Express error handler caught unknown middleware error',
       status: 500,
@@ -69,6 +49,6 @@ App.use((err, req, res, next) => {
 
 
 const port = process.env.port || 3001;
-App.listen(3001, () => console.log(`Listening on PORT ${port}}`))
+app.listen(3001, () => console.log(`Listening on PORT ${port}}`))
 
-module.exports = App;
+module.exports = app;
