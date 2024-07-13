bfcb6943e24f6c1cd2b94f92c2dda0fc407b9c94
Nathan Patterson <imnathanpatterson@gmail.com>
2024-07-13 15:16:52 -0400

    structuring frontned components

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import HomeLeft from '../components/HomeLeft.jsx';
+import ProfileTop from '../components/ProfileComponents/ProfileTop.jsx';
+import ProfileStats from '../components/ProfileComponents/ProfileStats.jsx';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className="profile-container">
+      <HomeLeft />
+      <ProfileTop />
+      <ProfileStats />
+    </div>
   )
 }
 
