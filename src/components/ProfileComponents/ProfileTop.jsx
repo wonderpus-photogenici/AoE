14bd069fc4ec01923b738170761599884bc23b35
Nathan Patterson <imnathanpatterson@gmail.com>
2024-07-16 21:48:39 -0400

    user profile structure laid out

--- src/components/ProfileComponents/ProfileTop.jsx
+++ src/components/ProfileComponents/ProfileTop.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
+import profPic from "../../Assets/aoelogo2.png";
+import leagueLogo from "../../Assets/leagueLogo.png";
+import valorantLogo from "../../Assets/valorantLogo.png";
+
 const ProfileTop = () => {
   return (
     <div className="profile-top">
-      ProfileTop
+    <div className="profile-top-container">
+      <img className="home-logo" src={profPic} alt="profile pic" />
+      <div className="username-addBtn-messageBtn">
+        <h1 className="profile-username">Username</h1>
+        <button className="profileBtn">Add</button>
+        <button className="profileBtn">Message</button>
+      </div>
+      <div className="location-gamelogos">
+        <h2 className="location">NA, West</h2>
+        <div className="game-logos">
+          <img src={valorantLogo} alt="Valorant Logo" />
+          <img src={leagueLogo} alt="League Logo" />
+        </div>
+      </div>
     </div>
-  )
-}
+  </div>
+  );
+};
 export default ProfileTop;
-
-
-
-
-
