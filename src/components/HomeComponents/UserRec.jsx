3fc7d13e2126eb467176c8868ffcffeb9d79c012
Kyler-Chiago <kchiago50@gmail.com>
2024-07-23 19:53:34 -0700

    new branch

--- src/components/HomeComponents/UserRec.jsx
+++ src/components/HomeComponents/UserRec.jsx
@@ -16,54 +16,36 @@ const UserRec = (props) => {
 
       <div className="userRec-userInfo">
         <h2 className="userRec-username">{username}</h2>
-        {/* <h4 className="userRec-hours">1.2k Hours</h4> */}
       </div>
 
       <div className="userRec-buttons">
         <button className="userRec-Button">Add</button>
         <button className="userRec-Button">Message</button>
       </div>
-
+      <div className="userRec-favorite-game">
+        Favorite Game:
+      </div>
       <div className="userRec-game-rank-Container">
-        Games: {allgames}
-        {/*  
-          <img className="userRec-gameLogo" src={leagueLogo} alt="League" />
-        <div className="userRec-rank">
-          <img className="userRec-gameRank" src={leagueDiamond} alt="League" />
-          <h5 className="userRec-rankName">Diamond</h5>
-        </div> */}
+        {/* Games: {allgames} */}
+        {/* Bio:  */}
+        {/* Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test
+        Hello this is a spam test */}
       </div>
 
     </div>
   )
-
-
-  // return (
-  //   <div className="userRec-wrapper">
-
-  //       <img className="userRec-prof-pic" src={profPicFiller} alt="" />
-
-  //     <div className="userRec-userInfo">
-  //       <h2 className="userRec-username">FelValencia</h2>
-  //       <h4 className="userRec-hours">1.2k Hours</h4>
-  //     </div>
-
-  //     <div className="userRec-buttons">
-  //       <button className="userRec-Button">Add</button>
-  //       <button className="userRec-Button">Message</button>
-  //     </div>
-
-  //     <div className="userRec-game-rank-Container">
-
-  //         <img className="userRec-gameLogo" src={leagueLogo} alt="League" />
-  //       <div className="userRec-rank">
-  //         <img className="userRec-gameRank" src={leagueDiamond} alt="League" />
-  //         <h5 className="userRec-rankName">Diamond</h5>
-  //       </div>
-  //     </div>
-
-  //   </div>
-  // )
 }
 
 export default UserRec
