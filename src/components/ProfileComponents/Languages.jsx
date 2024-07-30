53fb54ed581e7f44b964323d74578bab96c00587
Kyler-Chiago <kchiago50@gmail.com>
2024-07-30 02:45:54 -0700

    removed focus highlight on profile Location, Email, Languages

--- src/components/ProfileComponents/Languages.jsx
+++ src/components/ProfileComponents/Languages.jsx
@@ -4,7 +4,7 @@ const Languages = () => {
     return (
         <div className="languages">
             <div className="h3Mimic">Languages:</div>
-            <input type="text" name="profileLanguagesInput" id="profileLanguagesInput" className="profileInput"
+            <input type="text" name="profileLanguagesInput" id="profileLanguagesInput" className="profileInput noFocusHighlight"
             style={{border: '1px solid rgba(255, 255, 255, 0.4)', borderTop: 'none', borderLeft: 'none', borderRight: 'none'}}
             defaultValue={"temp"}></input>
         </div>
