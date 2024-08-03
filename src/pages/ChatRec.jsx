c40f30e236a19f8d736cc98a003f3543aae06ae2
Lia Hwang <ch646063@gmail.com>
2024-08-02 19:20:35 -0700

    need to update

--- src/pages/ChatRec.jsx
+++ src/pages/ChatRec.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Chat.css';
+import noPfp from '../Assets/noPfp.png';
 
 export default function ChatRec({msg, index, own}) {
     console.log('msg', msg, index)
     console.log('own here', own)
 
+    const CDNURL = "https://gusnjhjnuugqaqtgwhym.supabase.co/storage/v1/object/public/AoE/"
+
   return (
     <div className={own ? "messageHistory own": "messageHistory"} key={index}>
         <div className = "chatbox-top">
-            <img src='https://th.bing.com/th/id/OIP.5TUUOlD14Dc4wqneaqNP5AHaJU?rs=1&pid=ImgDetMain'         placeholder = "users pfp" className="chatUserPic"/>
+            <img src= {msg.pfp ? CDNURL + msg.pfp : 'url(' + noPfp +')'}  placeholder = "users pfp" className="chatUserPic"/>
             <p>{msg.sender}</p>
         </div>
         <div clasName="messagePortion">
